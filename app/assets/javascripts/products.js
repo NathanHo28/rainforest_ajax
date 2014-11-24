212dd00cd3d5877411c55ebcbd0693985098a912
NATHAN <nathan.ho28@gmail.com>
2014-11-24 14:54:19 -0500

    used getScript in order to have js requests

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,8 +2,4 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$.get('/products?search=' + searchValue)
-      .done(function(data){
-        console.log(data);
-        $('#products').html(data);
-      });
+$.getScript('/products?search=' + searchValue);
