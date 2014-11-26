58f12a853a77df3468df54a4c18a61f38d049310
NATHAN <nathan.ho28@gmail.com>
2014-11-26 10:59:16 -0500

    updated seeds.rb to remove images

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ User.create!({
   Product.create(
     name: Faker::Company.catch_phrase,
     description: "#{Faker::Company.bs} from #{Faker::Company.name} #{Faker::Company.suffix}",
-    price_in_cents: rand(100000)
+    price_in_cents: rand(100000),
   )
 end
 
